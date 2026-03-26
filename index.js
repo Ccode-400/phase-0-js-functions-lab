47676e55360e389974502a049a2af2aefe891e55
Ccode-400 <stanleymngige@gmail.com>
2026-03-26 04:11:54 +0300

    added code spacing

--- index.js
+++ index.js
@@ -20,9 +20,5 @@ function calculateDiscountedPrice(originalPrice, discountPercentage) {
   return originalPrice - discount;
 }
 
-
-
-
-
 // This is required for the test to function properly  
 module.exports = { calculateTax, convertToUpperCase, findMaximum, isPalindrome, calculateDiscountedPrice };
